8a1c8b78905d97b26261839145befd91944ae5ee
Marcos Damián Ezequiel Medina <marquitosmedina1@hotmail.com>
2023-11-06 23:31:43 -0300

    Basic app setup with tailwind

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <button className='px-5 py-1 text-xl font-sans hover:scale-110  transition rounded ease-out duration-300 outline font-bold text-gray-600 hover:text-blue-800'>Portfolio</button>
+     <p className='flow-root flex-col justify-center w-full'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quo perspiciatis debitis sit nulla est vel cumque animi numquam. Eligendi dolor illum nihil quod alias repellat ratione. Repellat, obcaecati quam! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum alias repellat voluptatibus. Asperiores, amet. Deserunt ut consequatur maxime? Consectetur, maiores dignissimos! Soluta eaque repudiandae voluptatem quas neque ex quod numquam!</p>
+    </>
+  )
+}
+
+export default App
