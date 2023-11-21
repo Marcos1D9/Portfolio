57a08da5f4c1d84b8424d1e059af4c96c9deaf8f
Marcos Damián Ezequiel Medina <marquitosmedina1@hotmail.com>
2023-11-20 23:04:26 -0300

    Web template

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,37 @@
 import './App.css'
+import { useEffect, useState } from "react";
+import DarkModeIcon from "/src/assets/DarkModeIcon.svg"
 
 function App() {
+  const [theme, setTheme] = useState(() => {
+    if (window.matchMedia("(prefers-color-scheme:dark)").matches) {
+      return "dark";
+    }
+    return "light";
+  })
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.querySelector("html").classList.add("dark");
+    } else {
+      document.querySelector("html").classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleChangeTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
   return (
-    <>
-     <button className='px-5 py-1 text-xl font-sans hover:scale-110  transition rounded ease-out duration-300 outline font-bold text-gray-600 hover:text-blue-800'>Portfolio</button>
-     <p className='flow-root flex-col justify-center w-full'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quo perspiciatis debitis sit nulla est vel cumque animi numquam. Eligendi dolor illum nihil quod alias repellat ratione. Repellat, obcaecati quam! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum alias repellat voluptatibus. Asperiores, amet. Deserunt ut consequatur maxime? Consectetur, maiores dignissimos! Soluta eaque repudiandae voluptatem quas neque ex quod numquam!</p>
-    </>
+    <div className="dark:bg-black w-screen h-screen">
+      <div className="w-screen h-16 flex flex-row justify-end align-top dark:bg-neutral-900">
+        <button className=" bg-slate-200 hover:bg-slate-300 px-4 py-2 rounded-full  dark:bg-green-600 dark:hover:bg-green-700"
+          onClick={handleChangeTheme}
+        >
+          <img className="w-8" src={DarkModeIcon} alt="DarkMode Icon" />
+        </button>
+      </div>
+    </div>
   )
 }
 
