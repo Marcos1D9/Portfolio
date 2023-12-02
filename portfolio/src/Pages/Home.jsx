2afbffd52733ad36af2dd994e08a3eb8976745dd
Marcos Damián Ezequiel Medina <marquitosmedina1@hotmail.com>
2023-12-02 18:15:15 -0300

    Presentation and experience section created

--- portfolio/src/Pages/Home.jsx
+++ portfolio/src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import DarkModeIcon from "/src/assets/DarkModeIcon.svg"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSun } from '@fortawesome/free-solid-svg-icons'
+import { faSun, faMoon, faBriefcase } from '@fortawesome/free-solid-svg-icons'
+import NewProfilePicWithBackground from '../assets/NewProfilePicWithBackground.png'
 
 const Home = () => {
     const [theme, setTheme] = useState(() => {
@@ -35,7 +35,7 @@ const Home = () => {
                             <FontAwesomeIcon icon={faSun} />
                         </div>
                         <div className="text-2xl hidden dark:block text-slate-100">
-                            <FontAwesomeIcon icon={faSun} />
+                            <FontAwesomeIcon icon={faMoon} />
                         </div>
                         {/* <img className="w-8" src={DarkModeIcon} alt="DarkMode Icon" /> */}
                     </button>
@@ -46,8 +46,70 @@ const Home = () => {
 
             {/* Main */}
             <div className="mx-auto max-w-3xl px-5 mb-5 flex flex-col gap-10"   >
-                <h1 className="text-white">Welcome to my portfolio</h1>
-                <h3>I'm Marcos Damián Medina, a front-end web developer</h3>
+                <main className="flex flex-col gap-16">
+                    {/* Hero presentation */}
+                    <section className="flex flex-col sm:flex-row items-center gap-5">
+
+                        {/* Profile Pic */}
+                        <div className="relative w-44 h-44 bg-none dark:bg-none border-8 border-white dark:border-gray-900/80 shadow-lg rounded-full overflow-hidden">
+                            <span>
+                                <img alt="Profile picture" src={NewProfilePicWithBackground} />
+                            </span>
+                        </div>
+                        {/* Profile Pic */}
+
+                        {/* About presentation */}
+                        <div className="flex flex-col items-center sm:items-start gap-3">
+
+                            {/* Presentation title */}
+                            <div className="flex flex-col items-left gap-2">
+                                <h1 className="text-2xl sm:text-4xl text-center sm:text-left font-bold text-slate-600 dark:text-slate-100">Marcos Damián E. Medina</h1>
+                                <p className="text-slate-700 text-sm sm:text-lg dark:text-slate-300 max-w-xl rounded-2xl">Welcome! I'm a passionate Web frontend developer</p>
+                            </div>
+                            {/* Presentation title */}
+
+                            {/* Social media Buttons */}
+                            <div className="flex gap-3"></div>
+                            {/* Social media Buttons */}
+                        </div>
+                        {/* About presentation */}
+
+                    </section>
+                    {/* Hero presentation */}
+                    <hr className="border border-dotted border-gray-300 dark:border-gray-600 rounded-md" />
+                    {/* Experience section */}
+                    <section className="w-full">
+
+                        {/* Experience section title */}
+                        <header className="font-bold text-2xl text-indigo-400 dark:text-gray-100 mb-6 flex items-center gap-3">
+                            <h2>Experience</h2>
+                            <FontAwesomeIcon className="text-3xl" icon={faBriefcase} />
+                        </header>
+                        {/* Experience section title */}
+
+                        {/* Experience Job title */}
+                        <div className="flex flex-col gap-10">
+                            <div className="flex flex-col gap-5">
+                                <div className="flex flex-col gap-1 ">
+                                    <h2 className="text-lg font-semibold text-slate-700 dark:text-slate-200">Front-End Developer • Munia - Salud Mental y Emcional en el Trabajo</h2>
+                                    <p className="text-sm text-slate-500 dark:text-slate-400">Sept. 2023 - Actualidad</p>
+                                </div>
+
+                                {/* Experience Job title */}
+
+                                {/* Experience Job description */}
+                                <ul className="list-disc flex flex-col gap-2 ml-5 text-slate-700 dark:text-gray-300 text-md">
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                                {/* Experience Job description */}
+                            </div>
+                        </div>
+                    </section>
+                    {/* Experience section */}
+                </main>
             </div>
             {/* Main */}
         </>
