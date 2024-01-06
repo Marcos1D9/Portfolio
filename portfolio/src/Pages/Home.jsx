e34c0553ec489c8a631dd5b64fd4f1bc119f6cd2
Marcos Damián Ezequiel Medina <marquitosmedina1@hotmail.com>
2024-01-06 01:10:27 -0300

    Email section Beta version

--- portfolio/src/Pages/Home.jsx
+++ portfolio/src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSun, faMoon, faBriefcase, faStar, faArrowUpRightFromSquare, faEnvelope, faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+import { faSun, faMoon, faBriefcase, faStar, faArrowUpRightFromSquare, faEnvelope, faPaperPlane, faCopy } from '@fortawesome/free-solid-svg-icons'
 import NewProfilePicWithBackground from '../assets/NewProfilePicWithBackground.png'
 import MuniaLoginPic from '../assets/MuniaLoginPic.jpg'
 import tailwindIcon from "../assets/tailwind-svgrepo-com.svg";
@@ -239,6 +239,7 @@ const Home = () => {
                             {/* Email Input */}
                             {/* Email Buttons */}
                             <div className="flex gap-2">
+                                {/* Send email button */}
                                 <div className="relative group/tooltip">
                                     <a href="mailto:marcosdamian054@gmail.com" target="_blank" rel="noreferrer" className="flex bg-indigo-100 hover:bg-opacity-80 dark:bg-indigo-500 hover:dark:bg-opacity-90 text-indigo-500 dark:text-indigo-100 py-2.5 px-4 rounded-lg">
                                         <div className="text-lg">
@@ -252,9 +253,22 @@ const Home = () => {
                                     before:rotate-45 before:h-2.5 before:-bottom-1 before:-z-10 before:left-1/2 
                                     before:-translate-x-1/2 before:dark:bg-white before:dark:gray-800 group-hover/tooltip:opacity-100 
                                     group-hover/tooltip:-translate-y-3">
-                                    Send me an Email
+                                        Send me an email
                                     </span>
                                 </div>
+                                {/* Send email button */}
+                                {/* Copy email button */}
+                                <div className="relative group/tooltip">
+                                    <button className="flex bg-indigo-100 hover:bg-opacity-80 dark:bg-indigo-500 hover:dark:bg-opacity-90 text-indigo-500 dark:text-indigo-100 py-2.5 px-4 rounded-lg">
+                                        <div className="text-lg">
+                                            <FontAwesomeIcon icon={faCopy} />
+                                        </div>
+                                    </button>
+                                    <span className="absolute pointer-events-none transition-all opacity-0 z-20 bottom-full -translate-y-0 py-1 px-1.5 text-xs left-1/2 -translate-x-1/2 rounded-md whitespace-nowrap text-gray-200 bg-gray-800 dark:bg-white dark:text-gray-700 before:content-[''] before:absolute before:bg-gray-800 before:rounded-sm before:w-2.5 before:rotate-45 before:h-2.5 before:-bottom-1 before:-z-10 before:left-1/2 before:-translate-x-1/2 before:dark:bg-white before:dark:gray-800 group-hover/tooltip:opacity-100 group-hover/tooltip:-translate-y-3">
+                                        Copy email
+                                    </span>
+                                </div>
+                                {/* Copy email button */}
                             </div>
                             {/* Email Buttons */}
                         </div>
