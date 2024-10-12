e9b95c984153a05f62f08ed5ff4025d0fce635b2
Marcos Damián Ezequiel Medina <marquitosmedina1@hotmail.com>
2024-10-12 16:59:52 -0300

    Copy email button removed

--- portfolio/src/Pages/Home.jsx
+++ portfolio/src/Pages/Home.jsx
@@ -558,35 +558,6 @@ const Home = () => {
                                     </span>
                                 </div>
                                 {/* Send email button */}
-                                {/* Copy email button */}
-                                <div className="relative group/tooltip">
-                                    <button className="flex bg-indigo-100 hover:bg-opacity-80 dark:bg-indigo-500 hover:dark:bg-opacity-90 text-indigo-500 dark:text-indigo-100 py-2.5 px-4 rounded-lg"
-                                        onClick={textToClipboard}
-                                    >
-                                        <div className="text-lg">
-                                            <FontAwesomeIcon icon={faCopy} />
-                                        </div>
-                                    </button>
-
-                                    <div
-                                        onMouseEnter={() => setShowTooltip(true)}
-                                        onMouseLeave={() => setShowTooltip(false)}
-                                        onClick={textToClipboard}
-                                        className="relative group"
-                                    >
-                                        <button className="your-button-class"><span className="absolute pointer-events-none transition-all opacity-0 z-20 bottom-full -translate-y-10 py-1 px-1.5 text-xs left-1/2 -translate-x-1/2 rounded-md whitespace-nowrap text-gray-200 bg-gray-800 dark:bg-white dark:text-gray-700 before:content-[''] before:absolute before:bg-gray-800 before:rounded-sm before:w-2.5 before:rotate-45 before:h-2.5 before:-bottom-1 before:-z-10 before:left-1/2 before:-translate-x-1/2 before:dark:bg-white before:dark:gray-800 group-hover/tooltip:opacity-100 group-hover/tooltip:-translate-y-14">
-                                            {language === 'en' ? "Click to copy my email" : 'Haz click para copiar mi email'}
-                                        </span></button>
-
-                                        {/* {showTooltip && (
-                                            
-                                        )} */}
-                                    </div>
-
-
-
-                                </div>
-                                {/* Copy email button */}
                             </div>
                             {/* Email Buttons */}
                         </div>
