d2f26ee53f07d7b5d0443cc486d2069bc6a87f70
Marcos Damián Ezequiel Medina <marquitosmedina1@hotmail.com>
2024-10-16 09:27:49 -0300

    Button border color fix
    Button mobile button tooltips removed and border added

--- portfolio/src/components/TooltipButton.jsx
+++ portfolio/src/components/TooltipButton.jsx
@@ -11,15 +11,25 @@ const TooltipButton = ({ link, henryTalent, henrySquare, githubIcon, linkedinIco
     <button className={`whitespace-nowrap max-phone:text-[8vw] ${henryTalent || henrySquare ? 'max-phone:flex max-phone:flex-col':''} max-phone:h-fit max-phone:whitespace-normal flex relative group/tooltip ${buttonFlexProps} gap-1 font-bold ${buttonXPadding} ${buttonWidth} ${buttonHeight} rounded-xl shadow-social-link dark:shadow-social-link-dark hover:shadow-social-link-hover dark:hover:shadow-social-link-hover-dark ${buttonTextColor} border-2 ${buttonBorderColor} dark:hover:border-2 ${buttonBackgroundColor} ${hoverButtonBorderColor} transition`}
       onClick={() => window.open(link, "_blank")}
     >
+      {/* Buttons Icons & images */}
       {henryTalent && <img src={HenryTalentIcon} className='h-7 dark:hidden max-phone:w-[16%] max-phone:h-[16%]' />}
+      
       {henrySquare && <img src={HenrySquareIcon} className='h-6 hidden dark:flex max-phone:w-[16%] max-phone:h-[16%]' />}
+      
       {githubIcon ? <FontAwesomeIcon icon={faGithub} /> : null}
+      
       {linkedinIcon ? <FontAwesomeIcon icon={faLinkedin} /> : null}
+      
+      {/* Main button text */}
       <h2 className='max-phone:text-[70%]'>{mainText}</h2>
-      <span className={`max-phone:text-[7vw] absolute border pointer-events-none transition-all opacity-0 dark:z-10 bottom-full -translate-y-0 py-2 px-2 text-xs left-1/2 -translate-x-1/2 rounded-md whitespace-nowrap ${tooltipBorder} ${tooltipTextColor} ${tooltipBackground} dark:border ${tooltipText} group-hover/tooltip:opacity-100 group-hover/tooltip:-translate-y-3 z-20`}>
+      
+      {/* Tooltip Text */}
+      <span className={`max-phone:text-[7vw] max-phone:hidden absolute border pointer-events-none transition-all opacity-0 dark:z-10 bottom-full -translate-y-0 py-2 px-2 text-xs left-1/2 -translate-x-1/2 rounded-md whitespace-nowrap ${tooltipBorder} ${tooltipTextColor} ${tooltipBackground} dark:border ${tooltipText} group-hover/tooltip:opacity-100 group-hover/tooltip:-translate-y-3 z-20`}>
         {tooltipSpan}
       </span>
-      <div className="max-phone:text-[7vw] -top-0 w-4 left-1/2 opacity-0 group-hover/tooltip:opacity-100 group-hover/tooltip:-translate-y-3 transition-all absolute">
+
+      {/* Tooltip pointer */}
+      <div className="max-phone:text-[7vw] max-phone:hidden -top-0 w-4 left-1/2 opacity-0 group-hover/tooltip:opacity-100 group-hover/tooltip:-translate-y-3 transition-all absolute">
         <div className={`h-3 w-3 absolute -left-1/2 rounded-sm ${pointerBackground} ${pointerBorder} dark:border-2 opacity-0 group-hover/tooltip:opacity-100 transition-all -rotate-45 transform origin-top-left`}></div>
       </div>
     </button>
